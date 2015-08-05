ba3ad26c10189878cbfcfaceef387b78ad69952a
Derek Bredensteiner <dbredensteiner@atlassian.com>
2015-08-05 11:17:06 -0700

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,28 @@
 var gulp = require('gulp')
 var babel = require('gulp-babel')
 var del = require('del')
+var run_sequence = require('run-sequence')
+var fs = require('fs')
 
 gulp.task("clean", function(cb){
   return del(['./build'], cb)
 })
 
-gulp.task("default", ["clean"], function(){
-  return gulp.src(["./src/gulp_tasks/**/*.js"])
+gulp.task("build", ["clean"], function(){
+  return gulp.src(["./src/gulpfile/**/*.js"])
     .pipe(babel())
     .on('error', function(err){
       console.error(err.fileName)
       console.error(err.loc)
       console.error(err.codeFrame)
     })
-    .pipe(gulp.dest("./build"))
+    .pipe(gulp.dest("./build/gulpfile"))
 })
+
+gulp.task("watch", function(){
+  return gulp.watch([
+    './src/gulp_tasks/**/*.js'
+  ], ["build"])
+})
+
+gulp.task("default", ["build","watch"])
