3913e20b87dc510bb486ee149dbfe1745a5d7ad2
Derek Bredensteiner <dbredensteiner@atlassian.com>
2015-08-12 22:41:27 -0700

    Use parent ref instead

--- src/client/index.js
+++ src/client/index.js
@@ -3,10 +3,6 @@ window.socket = require('./socket') // import syntax hoists erg ...
 m.child = function(which, extra){
   extra = extra || {}
   extra.parent = this
-  if(this.name&&this.name!=='root')
-    for(let i in this)
-      if(i !== 'children' && i !== 'parent')
-        extra[i] = this[i]
   return m.component(m.child.data[which], extra, which)
 }
 m.child.data = require('./tmp_modules')
